875c10f60e395f760315112e852e539892542370
ZbyszekMM <50945192+ZbyszekMM@users.noreply.github.com>
2020-12-11 20:34:15 +0100

    Corrected the git repo name.

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,6 +1,6 @@
 job('NodeJS example') {
     scm {
-        git('https://github.com/ZbyszekMM/jenkins-DSL-and-pipelines.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('thub.com/ZbyszekMM/jenkins-DSL-and-pipelines.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
             node / gitConfigEmail('jenkins-dsl@newtech.academy')
         }
